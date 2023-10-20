ca5ca1e031a5efc28d9409a5a8a824f0c0f6d88f
Mike <45664417+thewhobox@users.noreply.github.com>
2023-10-20 09:33:13 +0200

    changed exceptions to same in ofm

--- FileTransferException.cs
+++ FileTransferException.cs
@@ -4,24 +4,22 @@ public class FileTransferException : Exception
 {
     public int ErrorCode { get; }
     private static Dictionary<int, string> Messages = new Dictionary<int, string>() {
-        { 0x01, "LittleFS.begin() failed" },
-        { 0x02, "LittleFS.format() failed" },
-        { 0x03, "LittleFS not initialized" },
+        { 0x02, "Formatting of the file system has failed" },
         { 0x04, "Parameter pkg is greater than the allowed resultLength from Device" },
         //reserve
         { 0x41, "File already open" },
         { 0x42, "File can't be opened" },
         { 0x43, "File not opened" },
-        { 0x44, "File can't be deleted" },
-        { 0x45, "File can't be renamed" },
-        { 0x46, "File can't seek position" },
+        { 0x44, "Deleting of the file failed" },
+        { 0x45, "Renaming of the file failed" },
+        { 0x46, "The file can't seek to position" },
+        { 0x47, "File could not be written completely" },
         //reserve
         { 0x81, "Dir already open" },
         { 0x82, "Dir can't be opened" },
         { 0x83, "Dir not opened" },
-        { 0x84, "Dir can't be deleted" },
-        { 0x85, "Dir can't be created" },
-        { 0x86, "Dir has no more files" }
+        { 0x84, "Deleting of the folder failed" },
+        { 0x85, "Creation of the folder failed" },
     };
 
     public FileTransferException(int code) : base(Messages[code])
